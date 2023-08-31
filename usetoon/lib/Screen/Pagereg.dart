b55ab5536ce0acfcc6128d86e0eb8af3e8d3d2d9
phornchai Hompromma <phornchai@kkumail.com>
2023-08-31 23:18:41 +0700

    update form reg
    update reg
    V-0.2

--- usetoon/lib/Screen/Pagereg.dart
+++ usetoon/lib/Screen/Pagereg.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:usetoon/model/profile.dart';
 
 class RegPage extends StatelessWidget {
-  RegPage({super.key});
+  final formKey = GlobalKey<FormState>();
+  Profile profile =
+      Profile(username: '', email: '', password: '', confirmpassword: '');
 
   @override
   Widget build(BuildContext context) {
@@ -12,23 +15,41 @@ class RegPage extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(16.0),
         child: Form(
+          key: formKey,
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextFormField(
                   decoration: InputDecoration(labelText: "UserName"),
+                  onSaved: (String? username) {
+                    // เปลี่ยนเป็น String?
+                    profile.username = username ??
+                        ""; // ใช้ null-aware operator หรือกำหนดค่าเริ่มต้นให้เป็น ""
+                  },
                 ),
                 TextFormField(
                   decoration: InputDecoration(labelText: "Email"),
+                  keyboardType: TextInputType.emailAddress,
+                  onSaved: (String? email) {
+                    profile.email = email ?? "";
+                  },
                 ),
                 SizedBox(height: 16),
                 TextFormField(
                   decoration: InputDecoration(labelText: "password"),
+                  obscureText: true,
+                  onSaved: (String? password) {
+                    profile.password = password ?? "";
+                  },
                 ),
                 SizedBox(height: 16),
                 TextFormField(
                   decoration: InputDecoration(labelText: "confirm password"),
+                  obscureText: true,
+                  onSaved: (String? confirmPassword) {
+                    profile.confirmpassword = confirmPassword ?? "";
+                  },
                 ),
                 SizedBox(height: 16),
                 SizedBox(
@@ -38,7 +59,12 @@ class RegPage extends StatelessWidget {
                       "Submit",
                       style: TextStyle(fontSize: 20),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      formKey.currentState?.save();
+                      formKey.currentState?.reset();
+                      print(
+                          "User = ${profile.username} email = ${profile.email} password = ${profile.password} confirm password = ${profile.confirmpassword}");
+                    },
                   ),
                 )
               ],
--- usetoon/lib/model/profile.dart
+++ usetoon/lib/model/profile.dart
@@ -2,23 +2,23 @@ class Profile {
   String username; 
   String email;
   String password;
-  String confirm_password;
+  String confirmpassword;
 
   Profile({
     required this.username, 
     required this.email,
     required this.password,
-    required this.confirm_password,
+    required this.confirmpassword,
   });
 }
 
-void main() {
-  Profile user = Profile(
-    username: "username", 
-    email: "example@example.com",
-    password: "secured_password",
-    confirm_password: "secured_password",
-  );
+// void main() {
+//   Profile user = Profile(
+//     username: "username", 
+//     email: "example@example.com",
+//     password: "secured_password",
+//     confirm_password: "secured_password",
+//   );
 
 
-}
+// }
