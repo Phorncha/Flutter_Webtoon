60b1333c1fd89e3a282a5e0eff86d2ece383d545
phornchai Hompromma <phornchai@kkumail.com>
2023-09-02 00:23:24 +0700

    update
     Register  ติดปัญหา ในส่วนของ confirmPassword  ไม่สามารถทำงานได้ รอการแก้ไข

--- usetoon/lib/Screen/Pagereg.dart
+++ usetoon/lib/Screen/Pagereg.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:usetoon/model/profile.dart';
 
-class RegPage extends StatelessWidget {
+class RegPage extends StatefulWidget {
+  @override
+  _RegPageState createState() => _RegPageState();
+}
+
+class _RegPageState extends State<RegPage> {
   final formKey = GlobalKey<FormState>();
   Profile profile =
       Profile(username: '', email: '', password: '', confirmpassword: '');
+
+  bool _passwordVisible = false;
+  bool _confirmPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,34 +31,90 @@ class RegPage extends StatelessWidget {
                 TextFormField(
                   decoration: InputDecoration(labelText: "UserName"),
                   onSaved: (String? username) {
-                    // เปลี่ยนเป็น String?
-                    profile.username = username ??
-                        ""; // ใช้ null-aware operator หรือกำหนดค่าเริ่มต้นให้เป็น ""
+                    profile.username = username ?? "";
                   },
                 ),
+                //////////////////////////////////////////////////////
                 TextFormField(
                   decoration: InputDecoration(labelText: "Email"),
                   keyboardType: TextInputType.emailAddress,
+                  validator: (String? email) {
+                    if (email == null || email.isEmpty) {
+                      return 'ท่านยังไม่ได้ทำการป้อนอีเมล';
+                    }
+                    return null; // ส่งคืน null เมื่อข้อมูลถูกต้อง
+                  },
                   onSaved: (String? email) {
                     profile.email = email ?? "";
                   },
                 ),
                 SizedBox(height: 16),
                 TextFormField(
-                  decoration: InputDecoration(labelText: "password"),
-                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "รหัสผ่าน",
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  obscureText: !_passwordVisible,
+                  validator: (String? password) {
+                    if (password == null || password.isEmpty) {
+                      return "ท่านยังไม่ได้ทำการป้อนรหัสผ่าน";
+                    } else if (password.length < 6) {
+                      return "รหัสผ่านต้องมีความยาวอย่างน้อย 6 ตัวอักษร";
+                    } else if (!containsUppercaseLetter(password) ||
+                        !containsLowercaseLetter(password)) {
+                      return "รหัสผ่านต้องประกอบด้วยตัวอักษรตัวใหญ่และตัวเล็ก";
+                    }
+                    return null;
+                  },
                   onSaved: (String? password) {
                     profile.password = password ?? "";
                   },
                 ),
                 SizedBox(height: 16),
-                TextFormField(
-                  decoration: InputDecoration(labelText: "confirm password"),
-                  obscureText: true,
-                  onSaved: (String? confirmPassword) {
-                    profile.confirmpassword = confirmPassword ?? "";
-                  },
-                ),
+                ////////////////////////////////////////
+                // TextFormField(
+                //   decoration: InputDecoration(
+                //     labelText: "ยืนยันรหัสผ่าน",
+                //     suffixIcon: IconButton(
+                //       icon: Icon(
+                //         _confirmPasswordVisible
+                //             ? Icons.visibility
+                //             : Icons.visibility_off,
+                //         color: Colors.black,
+                //       ),
+                //       onPressed: () {
+                //         setState(() {
+                //           _confirmPasswordVisible = !_confirmPasswordVisible;
+                //         });
+                //       },
+                //     ),
+                //   ),
+                //   obscureText: !_confirmPasswordVisible,
+                //   validator: (String? confirmPassword) {
+                //     if (confirmPassword == null || confirmPassword.isEmpty) {
+                //       return "ท่านยังไม่ได้ทำการป้อนรหัสผ่าน";
+                //     } else if (confirmPassword.trim() !=
+                //         profile.password.trim()) {
+                //       return "รหัสผ่านไม่ตรงกัน";
+                //     }
+                //     return null;
+                //   },
+                //   onSaved: (String? confirmPassword) {
+                //     profile.confirmpassword = confirmPassword ?? "";
+                //   },
+                // ),
                 SizedBox(height: 16),
                 SizedBox(
                   width: double.infinity,
@@ -60,10 +124,19 @@ class RegPage extends StatelessWidget {
                       style: TextStyle(fontSize: 20),
                     ),
                     onPressed: () {
-                      formKey.currentState?.save();
-                      formKey.currentState?.reset();
-                      print(
-                          "User = ${profile.username} email = ${profile.email} password = ${profile.password} confirm password = ${profile.confirmpassword}");
+                      if (formKey.currentState!.validate()) {
+                        formKey.currentState?.save();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('ลงทะเบียนสำเร็จ'),
+                          ),
+                        );
+                        // print(
+                        //     "User = ${profile.username} email = ${profile.email} password = ${profile.password} confirmpassword = ${profile.password}");
+                        print(
+                            "User = ${profile.username} email = ${profile.email} password = ${profile.password}");
+                        formKey.currentState?.reset();
+                      }
                     },
                   ),
                 )
@@ -73,5 +146,13 @@ class RegPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  bool containsUppercaseLetter(String value) {
+    return value.contains(new RegExp(r'[A-Z]'));
+  }
+
+  bool containsLowercaseLetter(String value) {
+    return value.contains(new RegExp(r'[a-z]'));
   }
 }
--- usetoon/lib/model/profile.dart
+++ usetoon/lib/model/profile.dart
@@ -1,24 +1,22 @@
 class Profile {
-  String username; 
+  String username;
   String email;
   String password;
   String confirmpassword;
 
   Profile({
-    required this.username, 
+    required this.username,
     required this.email,
     required this.password,
     required this.confirmpassword,
   });
 }
 
-// void main() {
-//   Profile user = Profile(
-//     username: "username", 
-//     email: "example@example.com",
-//     password: "secured_password",
-//     confirm_password: "secured_password",
-//   );
-
-
-// }
+void main() {
+  Profile user = Profile(
+    username: "username",
+    email: "example@example.com",
+    password: "secured_password",
+    confirmpassword: "secured_password",
+  );
+}
