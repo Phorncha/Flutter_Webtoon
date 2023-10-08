cce9fe4514f3fd4f94df4aefab916111fa5d78c2
phornchai Hompromma <phornchai@kkumail.com>
2023-10-08 16:10:57 +0700

    update

--- apptoon/lib/Pages/homepage.dart
+++ apptoon/lib/Pages/homepage.dart
@@ -1,66 +1,130 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ข้อมูลจาก Firebase Firestore'),
+        title: Text("Homepage"),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('storys').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+      body: SingleChildScrollView(
+        child: Container(
+          height: 250,
+          child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            stream: FirebaseFirestore.instance.collection('storys').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text('เกิดข้อผิดพลาด: ${snapshot.error.toString()}'),
+                );
+              }
+              final documents = snapshot.data?.docs;
 
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('เกิดข้อผิดพลาด: ${snapshot.error.toString()}'),
-            );
-          }
+              if (documents == null || documents.isEmpty) {
+                return Center(
+                  child: Text('ไม่พบข้อมูลใน collection "storys"'),
+                );
+              }
 
-          final documents = snapshot.data?.docs;
+              return ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: documents.length,
+                itemBuilder: (context, index) {
+                  final document = documents[index];
+                  final data = document.data() as Map<String, dynamic>;
+                  final title = data['title'];
+                  final day = data['day'];
+                  final imageUrl = data['imageUrl'];
 
-          if (documents == null || documents.isEmpty) {
-            return Center(
-              child: Text('ไม่พบข้อมูลใน collection "storys"'),
-            );
-          }
-
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (context, index) {
-              final document = documents[index];
-              final data = document.data() as Map<String, dynamic>;
-              final title = data['title'];
-              final day = data['day'];
-              final id = data['id'];
-              final imageUrl = data['imageUrl'];
-
-              return ListTile(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Title: $title'),
-                    Text('Day: $day'),
-                    Text('ID: $id'),
-                    Image.network(
-                      imageUrl,
-                      width: 100,
-                      height: 200,
-                      fit: BoxFit.cover,
+                  return GestureDetector(
+                    onTap: () {
+                      _showImageDetails(context, title, day, imageUrl);
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      child: Card(
+                        elevation: 5,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              child: Image.network(
+                                imageUrl,
+                                width:
+                                    170, // You can adjust the width as needed
+                                height: 160,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Title: $title'),
+                                  Text('Day: $day'),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ],
-                ),
+                  );
+                },
               );
             },
-          );
-        },
+          ),
+        ),
       ),
+    );
+  }
+
+  void _showImageDetails(
+      BuildContext context, String title, String day, String imageUrl) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Image Details'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(imageUrl),
+              Text('Title: $title'),
+              Text('Day: $day'),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Next'),
+            )
+          ],
+        );
+      },
     );
   }
 }
